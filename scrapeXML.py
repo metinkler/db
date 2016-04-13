b51aff94faeb714c03fe61b518ce1f8752c2f8ac
Meghan Tinkler <metinkler@email.wm.edu>
2016-04-13 18:12:29 -0400

    Slight progress on XML scraper

--- scrapeXML.py
+++ scrapeXML.py
@@ -1,6 +1,18 @@
 import xmltodict as xmltd
 
+
+gameDict = {}
+
 def parseXML(xml):
-	gameDict = xmltodict.parse(xml, process_namespaces=True)
+	
+	# actual path to file
+	fd = open(xml)
+	gameDict = xmltodict.parse(fd.read())
 
+	return gameDict
 
+if __name__ == "__main__":
+
+	parseXML(sys.argv[1])
+
+	print(gameDict)
