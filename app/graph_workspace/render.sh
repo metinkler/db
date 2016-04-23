0b5d23fad4950803162a1abb148b4c13241e72ee
newOnahtaN <ncowen@email.wm.edu>
2016-04-23 18:04:13 -0400

    making render scripts

---
+++ app/graph_workspace/render.sh
@@ -0,0 +1,6 @@
+node --max-old-space-size=20000000 artgraph_creation.js
+node --max-old-space-size=20000000 desgraph_creation.js
+node --max-old-space-size=20000000 famgraph_creation.js
+node --max-old-space-size=20000000 gengraph_creation.js
+node --max-old-space-size=20000000 mechgraph_creation.js
+node --max-old-space-size=20000000 pubgraph_creation.js
