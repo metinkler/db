38bca3e05e8bb91f082cb87e6a3016afe4444c6e
newOnahtaN <ncowen@email.wm.edu>
2016-04-23 17:40:49 -0400

    updated data

--- app/graph_workspace/mechanics/mechmove.sh
+++ app/graph_workspace/mechanics/mechmove.sh
@@ -1,14 +1,14 @@
-rm /scratch/421/ncowen/galactic-data/nuget/2015-08-01T10-00-00Z/*
-cd /scratch/421/ncowen/galactic-data/nuget/2015-08-01T10-00-00Z/
+rm /scratch/421/ncowen/galactic-data/python/2015-09-27T13-00-00Z/*
+cd /scratch/421/ncowen/galactic-data/python/2015-09-27T13-00-00Z/
 git commit -am "removed data"
 git push origin gh-pages
-cd /scratch/421/ncowen/db/app/graph_workspace/publishers
-cp links.bin /scratch/421/ncowen/galactic-data/nuget/2015-08-01T10-00-00Z/
-cp labels.json /scratch/421/ncowen/galactic-data/nuget/2015-08-01T10-00-00Z/
-cp data/positions.bin /scratch/421/ncowen/galactic-data/nuget/2015-08-01T10-00-00Z/
-cd /scratch/421/ncowen/galactic-data/nuget/2015-08-01T10-00-00Z/
+cd /scratch/421/ncowen/db/app/graph_workspace/mechanics
+cp links.bin /scratch/421/ncowen/galactic-data/python/2015-09-27T13-00-00Z/
+cp labels.json /scratch/421/ncowen/galactic-data/python/2015-09-27T13-00-00Z/
+cp data/positions.bin /scratch/421/ncowen/galactic-data/python/2015-09-27T13-00-00Z/
+cd data/positions.bin /scratch/421/ncowen/galactic-data/python/2015-09-27T13-00-00Z/
 git add .
 git commit -am "updated data"
 git push origin gh-pages
 git push origin --delete gh-pages ; git push origin gh-pages
-cd /scratch/421/ncowen/db/app/graph_workspace/publishers
+cd /scratch/421/ncowen/db/app/graph_workspace/mechanics
