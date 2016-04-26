c8eeb4c002af7b56e29438b6ae2e14a7e2359e24
eobie3 <eobie3@aol.com>
2016-04-25 23:24:31 -0400

    Just aligned code

--- createDb.sql
+++ createDb.sql
@@ -1,7 +1,7 @@
 create table game
 	(name 		    varchar(256) 	not null unique,
 	synopsis    	text,
-	complexity	varchar(15),
+	complexity		varchar(15),
 	min_players 	int,
 	max_players 	int,
 	primary key(name));
@@ -19,83 +19,83 @@ CREATE TABLE board
     primary key(name)) INHERITS (game);
 
 CREATE TABLE card
-	(numCards	varchar(256),
-	suits		varchar(256),
-	refers	varchar(256),
-    num_players varchar(100),
-    url         text,
+	(numCards		varchar(256),
+	suits			varchar(256),
+	refers			varchar(256),
+    num_players 	varchar(100),
+    url         	text,
 	primary key(name)) INHERITS (game);
 
 CREATE TABLE domino
 	(numDom		    varchar(256),
 	domino_type 	varchar(100),
-	refers	varchar(256),
+	refers			varchar(256),
     num_players     varchar(100),
-    url         text,
+    url         	text,
 	primary key(name)) INHERITS (game);
 
 CREATE TABLE genre
-	(name		varchar(256)		not null unique,
+	(name			varchar(256)		not null unique,
 	primary key(name));
 
 CREATE TABLE publisher
-	(name		varchar(256)		not null unique,
+	(name			varchar(256)		not null unique,
 	primary key(name));
 
 CREATE TABLE mechanic
-	(name		varchar(256)		not null unique,
+	(name			varchar(256)		not null unique,
 	primary key(name));
 
 CREATE TABLE designer
-	(name		varchar(256)		not null unique,
+	(name			varchar(256)		not null unique,
 	primary key(name));
 
 CREATE TABLE artist
-	(name		varchar(256)		not null unique,
+	(name			varchar(256)		not null unique,
 	primary key(name));
 
 CREATE TABLE family
-	(name		varchar(256)		not null unique,
+	(name			varchar(256)		not null unique,
 	primary key(name));
 
 CREATE TABLE boardgenre
-	(boardName	varchar(256)		not null,
-	genreName	varchar(256)		not null,
+	(boardName		varchar(256)		not null,
+	genreName		varchar(256)		not null,
 	primary key(boardName, genreName),
 	foreign key(boardName) references board(name),
 	foreign key(genreName) references genre(name));
 
 CREATE TABLE boardpublisher
-	(boardName	varchar(256)		not null,
-	pubName		varchar(256)		not null,
+	(boardName		varchar(256)		not null,
+	pubName			varchar(256)		not null,
 	primary key(boardName, pubName),
 	foreign key(boardName) references board(name),
 	foreign key(pubName) references publisher(name));
 
 CREATE TABLE boardmechanic
-	(boardName	varchar(256)		not null,
+	(boardName		varchar(256)		not null,
 	mechName		varchar(256)		not null,
 	primary key(boardName, mechName),
 	foreign key(boardName) references board(name),
 	foreign key(mechName) references mechanic(name));
 
 CREATE TABLE boarddesigner
-	(boardName	varchar(256)		not null,
-	desName		varchar(256)		not null,
+	(boardName		varchar(256)		not null,
+	desName			varchar(256)		not null,
 	primary key(boardName, desName),
 	foreign key(boardName) references board(name),
 	foreign key(desName) references designer(name));
 
 CREATE TABLE boardartist
-	(boardName	varchar(256)		not null,
-	artName		varchar(256)		not null,
+	(boardName		varchar(256)		not null,
+	artName			varchar(256)		not null,
 	primary key(boardName, artName),
 	foreign key(boardName) references board(name),
 	foreign key(artName) references artist(name));
 
 CREATE TABLE boardfamily
-	(boardName	varchar(256)		not null,
-	famName		varchar(256)		not null,
+	(boardName		varchar(256)		not null,
+	famName			varchar(256)		not null,
 	primary key(boardName, famName),
 	foreign key(boardName) references board(name),
 	foreign key(famName) references family(name));
